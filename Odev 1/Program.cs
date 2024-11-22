c10f73c4875ef8eb272e54075e9de2da07a1fc3f
ismayil19005 <ismayil.piriyev1900@gmail.com>
2024-11-22 22:40:46 +0300

    Add files via upload

---
+++ Odev 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // List<int> oluşturuluyor ve içerisine birkaç tamsayı ekleniyor
+        List<int> sayilar = new List<int> { 5, 10, 20, 30 };
+
+        // Sayıların toplamı
+        int toplam = 0;
+
+        // foreach döngüsü ile liste elemanları yazdırılıyor
+        foreach (int sayi in sayilar)
+        {
+            Console.WriteLine(sayi);  // Sayıyı ekrana yazdır
+            toplam += sayi;           // Sayıyı toplam üzerine ekle
+        }
+
+        // Sayıların toplamı ekrana yazdırılıyor
+        Console.WriteLine("Toplam: " + toplam);
+    }
+}
